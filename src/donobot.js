c5959573409f16002860529e29ac5eb43f859a71
Meliodas <79414899+Meliodas-rai@users.noreply.github.com>
2021-03-22 20:12:21 -0300

    Add files via upload

---
+++ src/donobot.js
@@ -0,0 +1,18 @@
+const donosbot = (prefix) => {
+
+	return `
+*ESSES SÃO OS DONOS DE BOTS DO GRUPO*
+   *𝑪𝑨͢𝑼𝑺𝑺 - 𝑪𝑯͢𝑨𝑻 𝑫𝑶͢𝑺 𝑩𝑶𝑻𝒔*
+	          
+Causs: wa.me/554792091566   *O MAIS BRABO✓*
+
+Dark: wa.me/5522996215481     *BRABO ✓*
+
+S4anvic: wa.me/554498179556   *BRABO✓*    
+
+Guizão: wa.me/558598669655    *BRABO✓*
+
+Meliodas: wa.me/5589811246187 *pouco brabo✓*`
+}
+exports.donosbot = donosbot
+
