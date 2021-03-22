9f628ac8da1da73d58f188633b58110ee1e84473
Meliodas <79414899+Meliodas-rai@users.noreply.github.com>
2021-03-22 12:03:15 -0300

    Add files via upload

---
+++ src/help1.js
@@ -0,0 +1,45 @@
+const help1 = (prefix) => {
+
+	return `
+┣━━━━°❀ ❬ *CRIAR* ❭ ❀°━━━━┓
+┃
+⠀        (\__/)
+          (•ㅅ•) xandao irmao
+    ＿ノ ヽ ノ＼  __
+ /    `/ ⌒Ｙ⌒ Ｙ    ヽ
+(     (三ヽ人     /         |
+|    ﾉ⌒＼ ￣￣ヽ     ノ
+ヽ＿＿＿＞､＿＿_／
+         ｜( 王 ﾉ〈   
+         /ﾐ`ー―彡\  
+┣⊱❥ *xd*
+┣⊱❥ *pinterest*
+┣⊱❥ *anjing*
+┣⊱❥ *loli2*
+┣⊱❥ *anime*
+┣⊱❥ *wolflogo2 <texto | texto>*
+┣⊱❥ *quotemaker <tx | wtrmk | tema>*
+┣⊱❥ *galaxtext*
+┣⊱❥ *textdark*
+┣⊱❥ *textblue*
+┣⊱❥ *lovemake*
+┣⊱❥ *stiltext*
+┣⊱❥ *ninjalogo*
+┣⊱❥ *party*
+┣⊱❥ *rtext*
+┣⊱❥ *water*
+┣⊱❥ *lionlogo <texto | texto>*
+┣⊱❥ *textscreen*
+┣⊱❥ *text3d*
+┣⊱❥ *epep*
+┣⊱❥ *marvelogo <texto | texto>*
+┣⊱❥ *snow <texto | texto>*
+┣⊱❥ *firetext*
+
+════════════════════
+*Mᴇʟɪᴏᴅᴀs* 🤗
+*Digite dono para mais info*
+════════════════════`
+}
+exports.help1 = help1
+
