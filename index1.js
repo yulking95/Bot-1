23f227e2954d7367cceaca9359d940fef1102c64
Meliodas <79414899+Meliodas-rai@users.noreply.github.com>
2021-03-23 09:45:07 -0300

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,16 @@
+const {
+    WAConnection,
+    MessageType,
+    Presence,
+    Mimetype,
+    GroupSettingChange
+
+		if (messagesC.includes("bostão")){
+			client.updatePresence(from, Presence.composing)
+			reply("Vsfd")
+	}
+	
+					}
+	})
+}
+starts()
