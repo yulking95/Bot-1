0a836135c772a93d5ace6c3731789a145b641158
Meliodas <79414899+Meliodas-rai@users.noreply.github.com>
2021-03-27 09:16:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1061,8 +1061,8 @@ if (text.includes("placa"))
 					reply (mess.wait)
 					exec(`wget ${anu.result} -O ${cry} && ffmpeg -i ${cry} -vcodec libwebp -filter:v fps=fps=15 -lossless 1 -loop 0 -preset default -an -vsync 0 -s 512:512 ${rano}`, (err) => {
 						fs.unlinkSync(cry)
-						client.sendMessage(from, buffer, sticker, {quoted: mek})
 						tujuh = fs.readFileSync('./assets/peita.webp');
+						client.sendMessage(from, tujuh, sticker, {quoted: mek})
 						fs.unlinkSync(rano)
 					})
 					await limitAdd(sender) 
