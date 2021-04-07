dc001927ef7a31f2af8769c88189e5c3f71947f5
Meliodas <79414899+Meliodas-rai@users.noreply.github.com>
2021-04-06 21:50:34 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const { help } = require('./src/help')
 const { donosbot } = require('./src/donobot')
 const {help1} = require('./src/help1')
 const { modapk } = require('./src/modapk')
-const { myanimes } = require('./src/animesbrbr')
+const { animes } = require('./src/animesbrbr')
 const { pack } = require('./src/pack')
 const { outros } = require('./src/outros')
 const { vipmenu } = require('./src/vipmenu')
@@ -844,8 +844,9 @@ if (text.includes("placa"))
                     putagg = await getBuffer(`https://i.ibb.co/BcJ3bcJ/IMG-20210321-WA0162.jpg`)
                     client.sendMessage(from, putagg, image, {quoted: mek, caption: help(prefix)})
                     break
-                case 'myanimes':
-                    client.sendMessage(from, image, putagg, animesbrbr(prefix), text)
+                case 'animes':
+                    putagg = await getBuffer(`https://i.ibb.co/W0tHsKh/Safadao.png`)
+                    client.sendMessage(from, putagg, image, {quoted: mek, caption: animesbrbr(prefix)})
                     break
 				case 'help1':
 				case 'menu1':
