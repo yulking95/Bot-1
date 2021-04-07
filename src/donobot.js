5c28fed5154121ff7d7c278fccc776aa0ff4ecd4
Meliodas <79414899+Meliodas-rai@users.noreply.github.com>
2021-04-06 22:44:23 -0300

    Add files via upload

--- src/donobot.js
+++ src/donobot.js
@@ -1,18 +1,47 @@
-const donosbot = (prefix) => {
+const animelist = (prefix) => {
 
 	return `
-*ESSES SÃO OS DONOS DE BOTS DO GRUPO*
-   *𝑪𝑨͢𝑼𝑺𝑺 - 𝑪𝑯͢𝑨𝑻 𝑫𝑶͢𝑺 𝑩𝑶𝑻𝒔*
-	          
-Causs: wa.me/554792091566   *O MAIS BRABO✓*
+ 💪🏻𝙰𝚀𝚄𝙸 𝙴 𝙾 𝚇𝙰𝙽𝙳Ã𝙾 𝙿𝙾𝚁𝚁𝙰💪🏻
 
-Dark: wa.me/5522996215481     *BRABO ✓*
+ ╭╮╮╱▔▔▔▔╲╭╭╮ 
+ ╰╲╲▏▂╲╱▂▕╱╱╯
+ ┈┈╲▏▇▏▕▇▕╱┈┈
+ ┈┈╱╲▔▕▍▔╱╲┈┈ 
+ ╭╱╱ ▕╋╋╋╋▏╲╲╮
+ ╰╯╯┈╲▂▂╱┈╰╰ 
+ 
+  𝙿𝚎𝚒𝚝𝚊 𝚘𝚞 𝚁𝚎𝚜𝚙𝚎𝚒𝚝𝚊 𝙼𝚎𝚛𝚘 𝙼𝚘𝚛𝚝𝚊𝚕
+             HAHAHAHA😜
+ 
+ 👇 *Esta e a lista de animes que eu ja assisti:*👇
+  
+_Nanatsu No Taizai: conta a triste e engraçada historia de Meliodas e os Sete pecados capitais_
+https://animesonline.cc/anime/nanatsu-no-taizai-hd/
 
-S4anvic: wa.me/554498179556   *BRABO✓*    
 
-Guizão: wa.me/558598669655    *BRABO✓*
+_Undefeated Bahamut: conta a historia de Lux Arcadia que possui uma espada que o emcobre com a armadura mais forte do mundo_
+https://animesonline.games/b/animes/undefeated-bahamut-chronicle
 
-Meliodas: wa.me/5589811246187 *pouco brabo✓*`
+
+_No Game no Life: conta a historia de dois irmãos Shiro e Sora, eles são a dupla de jogadores invencíveis denominados de Kuhako_
+https://animesonline.cc/temporada/no-game-no-life-1-temporada-online/
+
+
+_DanMachi: conta a historia de Bell Cranell, sua deusa Hestia e seus amigos, o sonho dele e ser um Herói, ele conhece uma garota muito linda por quem se apaixona_
+https://animesonline.cc/anime/dungeon-ni-deai-o-motomeru-no-wa-machigatteiru-darou-ka/
+
+
+_Boku no Hero academia: Conta a história de Izuku Midorya, ele sonha em ser o herói mais poderoso de todos, mais ele nasceu sem um poder chamado de individualidade, mais ele gamha uma do seu herói preferido_
+https://www.myanimesonline.biz/animes/boku-no-hero-academia/
+
+
+_To Love Ru: conta a história PERVERTIDA e SORTUDA de Yuuki Rito, a sua SORTE começa quando uma alienígena GOSTOSA PRA PORRA aparece PELADA em sua banheira_, *n vou dar spoiler então vai assistir seu salafraio*
+https://animesync.biz/anime/to-love-ru-hd/
+
+SE DIVIRTA E MALHE SEU BRAÇO SE E QUE ME ENTENDE HEHEHE
+
+
+O último anime e pra tu malhar esse teu braço vey fino`
 }
-exports.donosbot = donosbot
+exports.animelist = animelist
 
