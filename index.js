376ba02c5abdaa8f9f2d30145c46ad0f7f2c1967
Meliodas <79414899+Meliodas-rai@users.noreply.github.com>
2021-04-23 12:53:14 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -876,8 +876,9 @@ if (text.includes("placa"))
 					break
 					case 'bejar':
 					if (!isGroup) return reply(mess.only.group)
+					if (!isGroup) return reply('Só em grupo caralho')
 						membr = []
-						teks = `Oloko, @${num.split('@')[0]} deu um beijo em @${sender.split("@s.whatsapp.net")[0]}`
+						teks = `Oloko, @${sender.split("@s.whatsapp.net")[0]} deu um beijo em @${mentioned.split("@s.whatsapp.net")[0]}`
 						mentions(teks, membr, true)
 					break
 
