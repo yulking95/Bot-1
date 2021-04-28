747a1614fecf8af2f75b448d71e62aa44ff5655c
Meliodas <79414899+Meliodas-rai@users.noreply.github.com>
2021-04-28 19:07:55 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -902,7 +902,7 @@ if (text.includes("placa"))
 					var cornos14 = cornos14[Math.floor(Math.random() * cornos14.length)]
 					var porcentagemcornos = ["1%", `2%`, `3%`, `4%`, `5%`, `6%`, `7`, `8%`, `9%`, `10`, `11%`, `12%`,`13%`, `14%`, `15%`, `16%`, `17%`, `18%`, `19%`, `20%`, `21%`, `22`, `23%`, `24%`, `25%`, `26%`, `27%`, `28%`, `27%`, `28%`, `29%`, `30%`, `31%`, `32%`, `33%`, `34%`, `35%`, `36%`, `37%`, `38%`, `39%`, `40%`, `41%`, `42%`, `43%`, `44%`, `45%`, `46%`, `47%`, `48%`, `49%`, `50%`, `51%`, `52%`, `53%`, `54%`, `55%`, `56%`, `57%`, `58%`, `59%`, `60%`, `61%`, `62%`, `63%`, `64%`, `65%`, `66%`, `67%`, `68%`, `69%`, `70%`, `71%`, `72%`, `73%`, `74%`, `75%`, `76%`, `77%`, `78%`, `79%`, `80%`, `81%`, `82%`, `85%`, `84%`, `85%`, `86%`, `87%`, `88%`, `89%`, `90%`, `91%`, `92%`, `93%`, `94%`, `95%`, `96%`, `97%`, `98%`, `99%`, `Esse ai tem o chifre batendo na lua ksksksks`]
 					const porcentagemcorno = porcentagemcornos[Math.floor(Math.random() * porcentagemcornos.length)]
-					Menssagem = `Sem pressão ${pushname} aqui e Xandão\nlista de cornos do grupo ${groupName}\nCornos:\n@${cornos5.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemcorno}\n@${cornos6.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemcorno}\n@${cornos7.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemcorno}\n@${cornos8.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemcorno}\n@${cornos9.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemcorno}\n@${cornos10.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemcorno}\n@${cornos11.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemcorno}\n\n???Cortesia do Super Xandão???`
+					teks = `Sem pressão ${pushname} aqui e Xandão\nlista de cornos do grupo ${groupName}\nCornos:\n@${cornos5.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemcorno}\n@${cornos6.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemcorno}\n@${cornos7.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemcorno}\n@${cornos8.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemcorno}\n@${cornos9.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemcorno}\n@${cornos10.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemcorno}\n@${cornos11.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemcorno}\n\n???Cortesia do Super Xandão???`
 					membr.push(corno1.jid)
 					membr.push(corno2.jid)
 					membr.push(corno3.jid)
@@ -913,7 +913,7 @@ if (text.includes("placa"))
 					membr.push(corno9.jid)
 					membr.push(corno10.jid)
 					membr.push(corno11.jid)
-					mentions(Menssagem, membr, true)
+					mentions(teks, membr, true)
 					break
 				case 'install':
 		if (!isGroup) return reply("este comando so pode ser usado em grupos")
