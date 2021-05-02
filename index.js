d59923ae3d92fd0c450db80e17a47ae8908efae1
Meliodas <79414899+Meliodas-rai@users.noreply.github.com>
2021-05-02 20:18:37 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1046,11 +1046,11 @@ if (text.includes("placa"))
 					var porcentagemcorno = ["1%", `2%`, `3%`, `4%`, `5%`, `6%`, `7`, `8%`, `9%`, `10`, `11%`, `12%`,`13%`, `14%`, `15%`, `16%`, `17%`, `18%`, `19%`, `20%`, `21%`, `22`, `23%`, `24%`, `25%`, `26%`, `27%`, `28%`, `27%`, `28%`, `29%`, `30%`, `31%`, `32%`, `33%`, `34%`, `35%`, `36%`, `37%`, `38%`, `39%`, `40%`, `41%`, `42%`, `43%`, `44%`, `45%`, `46%`, `47%`, `48%`, `49%`, `50%`, `51%`, `52%`, `53%`, `54%`, `55%`, `56%`, `57%`, `58%`, `59%`, `60%`, `61%`, `62%`, `63%`, `64%`, `65%`, `66%`, `67%`, `68%`, `69%`, `70%`, `71%`, `72%`, `73%`, `74%`, `75%`, `76%`, `77%`, `78%`, `79%`, `80%`, `81%`, `82%`, `85%`, `84%`, `85%`, `86%`, `87%`, `88%`, `89%`, `90%`, `91%`, `92%`, `93%`, `94%`, `95%`, `96%`, `97%`, `98%`, `99%`, `O chifre desse ai bate na lua ksksksk`]
 					const porcentagemc = porcentagemcorno[Math.floor(Math.random() * porcentagemcorno.length)]
 					teks = `${pushname} Esses são os cornos do grupo ${groupName}\n@${gays1.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemc}\n@${gays2.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemc}\n@${gays3.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemc}\n@${gays4.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemc}\n@${gays5.jid.split('@')[0]}\nCom uma porcentagem de ${porcentagemc}\n\n Sem pressão Neném, ⚡Xandão⚡ ta aqui`
-					membr.push(gays1.jid)
-					membr.push(gays2.jid)
-					membr.push(gays3.jid)
-					membr.push(gays4.jid)
-					membr.push(gays5.jid)
+					membr.push(cornos1.jid)
+					membr.push(cornos2.jid)
+					membr.push(cornos3.jid)
+					membr.push(cornos4.jid)
+					membr.push(cornos5.jid)
 					mentions(teks, membr, true)
 					break
                     case 'gostosa':
