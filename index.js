f50adb0433583c6c03ef2fb967c0b9956482d927
Meliodas <79414899+Meliodas-rai@users.noreply.github.com>
2021-05-12 14:21:57 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -880,19 +880,6 @@ if (text.includes("placa"))
 				case 'menu1':
 					client.sendMessage(from, help1(prefix), text)
 					break
-				case 'corno':
-				if (!isGroup) return reply(`Este comando só pode ser usado em grupos`)
-					if (mek.message.extendedTextMessage === undefined || mek.message.extendedTextMessage === null) return
-					mentioned = mek.message.extendedTextMessage.contextInfo.mentionedJid
-				const corno1 = groupMembers
-				const corno2 = groupMembers
-				const corno3 = groupMembers
-				const cornos1 = corno1(Math.floor[Math.random() * corno1.length])
-				const cornos2 = corno2(Math.floor[Math.random() * corno2.length])
-				const cornos3 = corno3(Math.floor[Math.random() * corno3.length])
-				var corno = ["1%", `2%`, `3%`, `4%`, `5%`, `6%`, `7`, `8%`, `9%`, `10`, `11%`, `12%`,`13%`, `14%`, `15%`, `16%`, `17%`, `18%`, `19%`, `20%`, `21%`, `22`, `23%`, `24%`, `25%`, `26%`, `27%`, `28%`, `27%`, `28%`, `29%`, `30%`, `31%`, `32%`, `33%`, `34%`, `35%`, `36%`, `37%`, `38%`, `39%`, `40%`, `41%`, `42%`, `43%`, `44%`, `45%`, `46%`, `47%`, `48%`, `49%`, `50%`, `51%`, `52%`, `53%`, `54%`, `55%`, `56%`, `57%`, `58%`, `59%`, `60%`, `61%`, `62%`, `63%`, `64%`, `65%`, `66%`, `67%`, `68%`, `69%`, `70%`, `71%`, `72%`, `73%`, `74%`, `75%`, `76%`, `77%`, `78%`, `79%`, `80%`, `81%`, `82%`, `85%`, `84%`, `85%`, `86%`, `87%`, `88%`, `89%`, `90%`, `91%`, `92%`, `93%`, `94%`, `95%`, `96%`, `97%`, `98%`, `99%`, `100%`]
-				const c = corno(Math.floor[Math.random() * corno.length])
-					break
 				case 'paus':
 				if (!isGroup) return reply(mess.only.group)
 				membr = []
