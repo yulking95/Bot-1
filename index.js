aeb9c62246894cc26714faff1826e0b565faada8
yulking95 <83798245+yulking95@users.noreply.github.com>
2021-05-15 02:10:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const {
     GroupSettingChange
 } = require('@adiwajshing/baileys')
 const { color, bgcolor } = requiere('./lib/color')
-const { eu } = require('./Fxc7/eu)
+const { eu } = require('./Fxc7/eu')
 const { help } = require('./src/help')
 const {help1} = require('./src/help1')
 const { modapk } = require('./src/modapk')
